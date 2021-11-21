b649b9fa59a10ed8e3dd9ad6cf60178df3b0e198
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2021-11-22 01:40:16 +0900

    プロジェクト ファイルを追加します。

---
+++ ProcessMonitor.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace ProcessMonitor
+{
+    partial class ProcessMonitor
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.data_grid_view_process = new System.Windows.Forms.DataGridView();
+            this.button_update_process_monitor = new System.Windows.Forms.Button();
+            this.timer_update_process_monitor = new System.Windows.Forms.Timer(this.components);
+            this.button_clear_process_monitor = new System.Windows.Forms.Button();
+            this.checkBox_auto_update = new System.Windows.Forms.CheckBox();
+            this.label_count = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.data_grid_view_process)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // data_grid_view_process
+            // 
+            this.data_grid_view_process.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.data_grid_view_process.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.data_grid_view_process.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.data_grid_view_process.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.data_grid_view_process.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.data_grid_view_process.Location = new System.Drawing.Point(12, 89);
+            this.data_grid_view_process.Name = "data_grid_view_process";
+            this.data_grid_view_process.ReadOnly = true;
+            this.data_grid_view_process.RowTemplate.Height = 21;
+            this.data_grid_view_process.Size = new System.Drawing.Size(776, 349);
+            this.data_grid_view_process.TabIndex = 0;
+            // 
+            // button_update_process_monitor
+            // 
+            this.button_update_process_monitor.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button_update_process_monitor.Location = new System.Drawing.Point(12, 12);
+            this.button_update_process_monitor.Name = "button_update_process_monitor";
+            this.button_update_process_monitor.Size = new System.Drawing.Size(75, 43);
+            this.button_update_process_monitor.TabIndex = 1;
+            this.button_update_process_monitor.Text = "UPDATE";
+            this.button_update_process_monitor.UseVisualStyleBackColor = false;
+            this.button_update_process_monitor.Click += new System.EventHandler(this.button_update_process_monitor_Click);
+            // 
+            // timer_update_process_monitor
+            // 
+            this.timer_update_process_monitor.Enabled = true;
+            this.timer_update_process_monitor.Interval = 30000;
+            this.timer_update_process_monitor.Tick += new System.EventHandler(this.timer_update_process_monitor_Tick);
+            // 
+            // button_clear_process_monitor
+            // 
+            this.button_clear_process_monitor.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button_clear_process_monitor.Location = new System.Drawing.Point(93, 12);
+            this.button_clear_process_monitor.Name = "button_clear_process_monitor";
+            this.button_clear_process_monitor.Size = new System.Drawing.Size(75, 43);
+            this.button_clear_process_monitor.TabIndex = 2;
+            this.button_clear_process_monitor.Text = "CLEAR";
+            this.button_clear_process_monitor.UseVisualStyleBackColor = false;
+            this.button_clear_process_monitor.Click += new System.EventHandler(this.button_clear_process_monitor_Click);
+            // 
+            // checkBox_auto_update
+            // 
+            this.checkBox_auto_update.AutoSize = true;
+            this.checkBox_auto_update.Checked = true;
+            this.checkBox_auto_update.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_auto_update.Location = new System.Drawing.Point(174, 39);
+            this.checkBox_auto_update.Name = "checkBox_auto_update";
+            this.checkBox_auto_update.Size = new System.Drawing.Size(104, 16);
+            this.checkBox_auto_update.TabIndex = 3;
+            this.checkBox_auto_update.Text = "AUTO UPDATE";
+            this.checkBox_auto_update.UseVisualStyleBackColor = true;
+            // 
+            // label_count
+            // 
+            this.label_count.AutoSize = true;
+            this.label_count.Location = new System.Drawing.Point(12, 74);
+            this.label_count.Name = "label_count";
+            this.label_count.Size = new System.Drawing.Size(50, 12);
+            this.label_count.TabIndex = 4;
+            this.label_count.Text = "COUNT: ";
+            // 
+            // ProcessMonitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_count);
+            this.Controls.Add(this.checkBox_auto_update);
+            this.Controls.Add(this.button_clear_process_monitor);
+            this.Controls.Add(this.button_update_process_monitor);
+            this.Controls.Add(this.data_grid_view_process);
+            this.Name = "ProcessMonitor";
+            this.Text = "Process Monitor";
+            this.Load += new System.EventHandler(this.ProcessMonitor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.data_grid_view_process)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView data_grid_view_process;
+        private System.Windows.Forms.Button button_update_process_monitor;
+        private System.Windows.Forms.Timer timer_update_process_monitor;
+        private System.Windows.Forms.Button button_clear_process_monitor;
+        private System.Windows.Forms.CheckBox checkBox_auto_update;
+        private System.Windows.Forms.Label label_count;
+    }
+}
+
---
+++ ProcessMonitor.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProcessMonitor
+{
+    public partial class ProcessMonitor : Form
+    {
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, uint nSize, string lpFileName);
+
+        [DllImport("kernel32.dll")]
+        private static extern int GetPrivateProfileInt(string lpAppName, string lpKeyName, int nDefault, string lpFileName);
+        public ProcessMonitor()
+        {
+            InitializeComponent();
+
+            timer_update_process_monitor.Interval = GetPrivateProfileInt("SETTING", "timer_interval", 30000, ".\\setting.ini");
+        }
+
+        private void ProcessMonitor_Load(object sender, EventArgs e)
+        {
+            data_grid_view_process.ColumnCount = 3;
+            data_grid_view_process.Columns[0].HeaderText = "ID";
+            data_grid_view_process.Columns[1].HeaderText = "ProcessId";
+            data_grid_view_process.Columns[2].HeaderText = "CommandLine";
+        }
+
+        private void button_update_process_monitor_Click(object sender, EventArgs e)
+        {
+            data_grid_view_process.ColumnCount = 3;
+            data_grid_view_process.Columns[0].HeaderText = "ID";
+            data_grid_view_process.Columns[1].HeaderText = "ProcessId";
+            data_grid_view_process.Columns[2].HeaderText = "CommandLine";
+
+            int capacitySize = 256;
+            StringBuilder string_builder = new StringBuilder(capacitySize);
+            GetPrivateProfileString("SETTING", "command_line_like", "", string_builder, Convert.ToUInt32(string_builder.Capacity), ".\\setting.ini");
+
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher(string.Format("SELECT * FROM Win32_Process WHERE CommandLine like '%{0}%'", string_builder.ToString())))
+            using (ManagementObjectCollection objects = searcher.Get())
+            {
+                data_grid_view_process.RowCount = objects.Count;
+                int row = 0;
+                foreach (var obj in objects)
+                {
+                    try
+                    {
+                        data_grid_view_process.Rows[row].Cells[0].Value = row + 1;
+                        data_grid_view_process.Rows[row].Cells[1].Value = obj["ProcessId"]?.ToString();
+                        data_grid_view_process.Rows[row].Cells[2].Value = obj["CommandLine"]?.ToString();
+                        row += 1;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: {0}", ex.Message);
+                    }
+                }
+                label_count.Text = string.Format("Count: {0}", row.ToString());
+            }
+        }
+
+        private void timer_update_process_monitor_Tick(object sender, EventArgs e)
+        {
+            if (checkBox_auto_update.Checked)
+            {
+                button_clear_process_monitor_Click(sender, e);
+                button_update_process_monitor_Click(sender, e);
+            }
+        }
+
+        private void button_clear_process_monitor_Click(object sender, EventArgs e)
+        {
+            label_count.Text = string.Format("Count: ");
+            data_grid_view_process.Rows.Clear();
+        }
+    }
+}
